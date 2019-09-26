e9afedae6cb4ad4ab7aa94465ba81bdc1a8b0366
alberto <jorgealbertomancera@outlook.com>
2019-09-26 14:34:00 -0500

    upd

--- src/App.js
+++ src/App.js
@@ -1,49 +1,262 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+//import './App.css';
+
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 import moment from 'moment'
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
+const DragAndDropCalendar = withDragAndDrop(Calendar)
 const localizer = momentLocalizer(moment);
+const now = new Date()
+class EventComponent extends React.Component {
+  render() {
+    return (
+      <img
+        alt="sc"
+        src="http://www.financiatrucks.com.mx/images/testimonials/ts-author.jpg"
+      />
+    );
+  }
+}
+const  events = [
+  {
+    id: 0,
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: new Date(2015, 3, 0),
+    end: new Date(2015, 3, 1),
+  },
+  {
+    id: 1,
+    title: 'Long Event',
+    start: new Date(2015, 3, 7),
+    end: new Date(2015, 3, 10),
+  },
 
-const MyCalendar = props => (
-  <div>
-    <Calendar
-      localizer={localizer}
-      events={[]}
-      startAccessor="start"
-      endAccessor="end"
-    />
-  </div>
-)
+  {
+    id: 2,
+    title: 'DTS STARTS',
+    start: new Date(2016, 2, 13, 0, 0, 0),
+    end: new Date(2016, 2, 20, 0, 0, 0),
+  },
+
+  {
+    id: 3,
+    title: 'DTS ENDS',
+    start: new Date(2016, 10, 6, 0, 0, 0),
+    end: new Date(2016, 10, 13, 0, 0, 0),
+  },
+
+  {
+    id: 4,
+    title: 'Some Event',
+    start: new Date(2015, 3, 9, 0, 0, 0),
+    end: new Date(2015, 3, 10, 0, 0, 0),
+  },
+  {
+    id: 5,
+    title: 'Conference',
+    start: new Date(2015, 3, 11),
+    end: new Date(2015, 3, 13),
+    desc: 'Big conference for important people',
+  },
+  {
+    id: 6,
+    title: 'Meeting',
+    start: new Date(2015, 3, 12, 10, 30, 0, 0),
+    end: new Date(2015, 3, 12, 12, 30, 0, 0),
+    desc: 'Pre-meeting meeting, to prepare for the meeting',
+  },
+  {
+    id: 7,
+    title: 'Lunch',
+    start: new Date(2015, 3, 12, 12, 0, 0, 0),
+    end: new Date(2015, 3, 12, 13, 0, 0, 0),
+    desc: 'Power lunch',
+  },
+  {
+    id: 8,
+    title: 'Meeting',
+    start: new Date(2015, 3, 12, 14, 0, 0, 0),
+    end: new Date(2015, 3, 12, 15, 0, 0, 0),
+  },
+  {
+    id: 9,
+    title: 'Happy Hour',
+    start: new Date(2015, 3, 12, 17, 0, 0, 0),
+    end: new Date(2015, 3, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day',
+  },
+  {
+    id: 10,
+    title: 'Dinner',
+    start: new Date(2015, 3, 12, 20, 0, 0, 0),
+    end: new Date(2015, 3, 12, 21, 0, 0, 0),
+  },
+  {
+    id: 11,
+    title:  <img
+    alt="sc"
+    src="http://www.financiatrucks.com.mx/images/testimonials/ts-author.jpg"
+  />,
+    subtitle:'something',
+    start: new Date(2015, 3, 13, 7, 0, 0),
+    end: new Date(2015, 3, 13, 10, 30, 0),
+  },
+  {
+    id: 12,
+    title: <img
+    alt="sc"
+    src="https://www.your-homeservices.com/wp-content/uploads/2019/05/icon8.png"
+  />,
+    start: new Date(2015, 3, 17, 19, 30, 0),
+    end: new Date(2015, 3, 18, 2, 0, 0),
+  },
+  {
+    id: 12.5,
+    title: 'Late Same Night Event',
+    start: new Date(2015, 3, 17, 19, 30, 0),
+    end: new Date(2015, 3, 17, 23, 30, 0),
+  },
+  {
+    id: 13,
+    title: 'Multi-day Event',
+    start: new Date(2015, 3, 20, 19, 30, 0),
+    end: new Date(2015, 3, 22, 2, 0, 0),
+  },
+  {
+    id: 14,
+    title: 'Today',
+    start: new Date(new Date().setHours(new Date().getHours() - 3)),
+    end: new Date(new Date().setHours(new Date().getHours() + 3)),
+  },
+  {
+    id: 15,
+    title: 'Point in Time Event',
+    start: now,
+    end: now,
+  },
+]
+// const MyCalendar = props => (
+//   <div>
+//     <Calendar
+//     selectable
+//       localizer={localizer}
+//       events={events}
+//         onEventDrop={this.moveEvent}
+//         resizable
+//         onEventResize={this.resizeEvent}
+//         onSelectSlot={this.newEvent}
+//         onDragStart={console.log}
+//         defaultView={Views.MONTH}
+//         defaultDate={new Date(2015, 3, 12)}
+//       startAccessor="start"
+//       endAccessor="end"
+//     />
+//   </div>
+// )
 
 
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      events: events,
+    }
+
+    this.moveEvent = this.moveEvent.bind(this)
+    this.newEvent = this.newEvent.bind(this)
+  }
+  moveEvent({ event, start, end, isAllDay: droppedOnAllDaySlot }) {
+    const { events } = this.state
+
+    const idx = events.indexOf(event)
+    let allDay = event.allDay
+
+    if (!event.allDay && droppedOnAllDaySlot) {
+      allDay = true
+    } else if (event.allDay && !droppedOnAllDaySlot) {
+      allDay = false
+    }
+
+    const updatedEvent = { ...event, start, end, allDay }
+
+    const nextEvents = [...events]
+    nextEvents.splice(idx, 1, updatedEvent)
+
+    this.setState({
+      events: nextEvents,
+    })
+
+    // alert(`${event.title} was dropped onto ${updatedEvent.start}`)
+  }
+
+  resizeEvent = ({ event, start, end }) => {
+    const { events } = this.state
+
+    const nextEvents = events.map(existingEvent => {
+      return existingEvent.id === event.id
+        ? { ...existingEvent, start, end }
+        : existingEvent
+    })
+
+    this.setState({
+      events: nextEvents,
+    })
+
+    //alert(`${event.title} was resized to ${start}-${end}`)
+  }
+
+  newEvent(event) {
+    // let idList = this.state.events.map(a => a.id)
+    // let newId = Math.max(...idList) + 1
+    // let hour = {
+    //   id: newId,
+    //   title: 'New Event',
+    //   allDay: event.slots.length == 1,
+    //   start: event.start,
+    //   end: event.end,
+    // }
+    // this.setState({
+    //   events: this.state.events.concat([hour]),
+    // })
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+    
 
-        <MyCalendar>
-        </MyCalendar>
+        {/* <MyCalendar
+        events={this.state.events}
+        >
+        </MyCalendar> */}
+            <DragAndDropCalendar
+    selectable
+      localizer={localizer}
+      events={this.state.events}
+        onEventDrop={this.moveEvent}
+        resizable
+        onEventResize={this.resizeEvent}
+        onSelectSlot={this.newEvent}
+        onDragStart={console.log}
+        defaultView={Views.MONTH}
+        defaultDate={new Date(2015, 3, 12)}
+        // components={{
+        //   event: EventComponent
+        // }}
+      startAccessor="start"
+      endAccessor="end"
+    />
+        
       </header>
     </div>
-  );
+  );}
 }
 
 export default App;
